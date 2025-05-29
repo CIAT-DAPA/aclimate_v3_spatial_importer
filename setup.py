0cac444803c16f7f43829b5db262a4a4d4c7e887
Daniel Esteban Guzmán <estebantoon@hotmail.com>
2025-05-29 11:26:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setup(
     download_url="https://github.com/CIAT-DAPA/aclimate_v3_spatial_importer",
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    include_package_data=True,
+    package_data={
+        "aclimate_v3_spatial_importer": ["conf/*"],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
